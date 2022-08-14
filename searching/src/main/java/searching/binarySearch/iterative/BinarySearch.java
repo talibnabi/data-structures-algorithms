b69ddaf67c63cdd9801876c9dfc5eeca406c3016
talibnabi <talibnabiyev@gmail.com>
2022-08-14 06:53:58 -0700

    commit

--- searching/src/main/java/searching/binarySearch/iterative/BinarySearch.java
+++ searching/src/main/java/searching/binarySearch/iterative/BinarySearch.java
@@ -1,15 +1,4 @@
 package searching.binarySearch.iterative;
 
 public class BinarySearch {
-    private int binarySearch(int[] arr, int left, int right, int element) {
-        if (right >= left) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] == element)
-                return mid;
-            if (arr[mid] > element)
-                return binarySearch(arr, left, mid - 1, element);
-            return binarySearch(arr, mid + 1, right, element);
-        }
-        return -1;
-    }
 }
