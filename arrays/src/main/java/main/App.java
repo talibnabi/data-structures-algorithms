5914acbb86f59dfd76717d0f8ff02ce9e9a43a59
talibnabi <talibnabiyev@gmail.com>
2022-09-08 02:47:06 -0700

    commit

--- arrays/src/main/java/main/App.java
+++ arrays/src/main/java/main/App.java
@@ -1,7 +1,11 @@
 package main;
 
 public class App {
-    ///Code complexity -> Big-O,Omega,Theta
+    //Code complexity -> Big-O,Omega,Theta
+    //Time complexity -> O(1),O(log n),O(n),O(n(log n)),O(n2),O(2n),O(n!)
+    //Bubble Sort ->O(n2)   Heap sort->O(n(log n))
+
+    //C0+n*n*C1   C0+n*C1
     public static void main(String[] args) {
 
     }
