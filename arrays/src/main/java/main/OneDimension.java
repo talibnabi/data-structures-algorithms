6fbfe397f0651266b98cc5e594e97c389b1119f3
talibnabi <talibnabiyev@gmail.com>
2022-09-08 04:42:05 -0700

    commit

---
+++ arrays/src/main/java/main/OneDimension.java
@@ -0,0 +1,21 @@
+package main;
+
+
+import java.util.Arrays;
+
+public class OneDimension {
+    // 1.search   O(n)
+    // 2.access   O(1)
+    // 3.insert   O(n)
+    // 4.delete   O(n)
+    public static void main(String[] args) {
+//        int[] arr = new int[3];
+//        arr[0] = 2;
+//        arr[1] = 4;
+//        arr[2] = 6;
+//        System.out.println(Arrays.toString(Arrays.stream(arr).toArray()));
+//        arr[2]=34;
+//        System.out.println(Arrays.toString(Arrays.stream(arr).toArray()));
+
+    }
+}
