af670cceb2e548ea53577c993bfc9852a47976e5
talibnabi <talibnabiyev@gmail.com>
2022-09-08 07:38:55 -0700

    commit

--- arrays/src/main/java/main/Interview2.java
+++ arrays/src/main/java/main/Interview2.java
@@ -4,8 +4,31 @@ public class Interview2 {
 
 
     public static void main(String[] args) {
-        int area = largestContainer(new int[]{1, 2, 3, 4, 5});
-        System.out.println(area);
+//        int area = largestContainer(new int[]{1, 2, 3, 4, 5});
+//        System.out.println(area);
+        int longest = longestOnes(new int[]{0, 1, 1, 0, 1, 0, 1});
+        System.out.println(longest);
+    }
+
+    private static int longestOnes(int[] array) {
+        int longestOne = Integer.MIN_VALUE;
+        int beforeZero = 0;
+        int afterZero = 0;
+        for (int j = 0; j < array.length - 1; j++) {
+            if (array[j] == 1) {
+                afterZero++;
+                if (j == array.length - 1) {
+                    longestOne = Math.max(longestOne, afterZero + beforeZero + 1);
+                }
+            } else {
+                int len = afterZero + beforeZero + 1;
+                longestOne = Math.max(longestOne, len);
+                beforeZero = afterZero;
+                afterZero = 0;
+            }
+        }
+//        longestOne = Math.max(longestOne, afterZero + beforeZero + 1);
+        return longestOne;
     }
 
     private static int largestContainer(int[] array) {
