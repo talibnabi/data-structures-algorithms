affcdd6c3483a1154423af1875e9e33d30f8d35d
talibnabi <talibnabiyev@gmail.com>
2022-09-08 06:11:59 -0700

    commit

--- arrays/src/main/java/main/Interview2.java
+++ arrays/src/main/java/main/Interview2.java
@@ -1,7 +1,28 @@
 package main;
 
 public class Interview2 {
-    public static void main(String[] args) {
 
+
+    public static void main(String[] args) {
+        int area = largestContainer(new int[]{2, 9, 1, 2, 5});
+        System.out.println(area);
+    }
+
+    private static int largestContainer(int[] array) {
+        int maxArea = Integer.MIN_VALUE;
+        int begin = 0;
+        int end = array.length - 1;
+        while (begin < end) {
+            int width = end - begin;
+            int height = Math.min(array[begin], array[end]);
+            int area = width * height;
+            maxArea = Math.max(maxArea, area);
+            if (array[begin] < array[end]) {
+                begin++;
+            } else {
+                end--;
+            }
+        }
+        return maxArea;
     }
 }
