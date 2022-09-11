835e2b2fba09050e7306ba335a2b8136e3b85dd7
talibnabi <talibnabiyev@gmail.com>
2022-09-11 08:56:51 -0700

    commit

--- arrays/src/main/java/main/amazonInterviewLongestSequence/LongestSequence.java
+++ arrays/src/main/java/main/amazonInterviewLongestSequence/LongestSequence.java
@@ -4,41 +4,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LongestSequence {
+    private static final int[] array = {1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1};
 
     public static void main(String[] args) {
+        System.out.println(longestSequence());
+    }
 
-        int[] arr = {1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1};//input
-        //first convert the problem
-        List<Integer> list = new ArrayList<Integer>();
+    private static int longestSequence() {
+        List<Integer> integerList = getList(array);
+        System.out.println(integerList);
+        int totalNum = 0;
+        for (int i = 0; i < integerList.size(); i++) {
+            int numSum = integerList.get(i);
+            if ((i + 1) < integerList.size()) {
+                numSum += 1;
+            }
+            if ((i + 2) < integerList.size()) {
+                numSum += integerList.get(i + 2);
+            }
+            if (numSum > totalNum) {
+                totalNum = numSum;
+            }
+        }
+        return totalNum;
+    }
+
+    private static List<Integer> getList(int[] array) {
         int sequenceOnesTotal = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 1) {
+        List<Integer> nums = new ArrayList<Integer>();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 1) {
                 sequenceOnesTotal++;
-                if (i == arr.length - 1) {
-                    list.add(sequenceOnesTotal);
+                if (sequenceOnesTotal > 0 && i == array.length - 1) {
+                    nums.add(sequenceOnesTotal);
                 }
             } else {
                 if (sequenceOnesTotal > 0) {
-                    list.add(sequenceOnesTotal);
+                    nums.add(sequenceOnesTotal);
                 }
-                list.add(arr[i]);
+                nums.add(array[i]);
                 sequenceOnesTotal = 0;
             }
         }
-        System.out.println(list);
-        int maxSequence = 0;
-        for (int i = 0; i < list.size(); i += 2) {
-            int len = list.get(i);
-            if ((i + 1) < list.size()) {
-                len += 1;
-            }
-            if ((i + 2) < list.size()) {
-                len += list.get(i + 2);
-            }
-            if (len > maxSequence) {
-                maxSequence = len;
-            }
-        }
-        System.out.println("maxSequence: " + maxSequence);
+        return nums;
     }
 }
