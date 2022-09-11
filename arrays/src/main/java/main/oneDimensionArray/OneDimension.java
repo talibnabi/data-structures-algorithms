6102f3e5ef08a2df3c9fbae301bb56284ce41e5b
talibnabi <talibnabiyev@gmail.com>
2022-09-11 09:23:09 -0700

    commit

---
+++ arrays/src/main/java/main/oneDimensionArray/OneDimension.java
@@ -0,0 +1,13 @@
+package main.oneDimensionArray;
+
+public class OneDimension {
+    /*
+    1.search ->O(n)
+    2.access ->O(1)
+    3.insert ->O(n)
+    4.delete ->O(n)
+    * */
+    public static void main(String[] args) {
+
+    }
+}
