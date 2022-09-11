bad6ceab561410e8c55184f9c15d3faa93c9a29c
talibnabi <talibnabiyev@gmail.com>
2022-09-11 09:43:42 -0700

    commit

--- arrays/src/main/java/main/oneDimensionArray/OneDimension.java
+++ arrays/src/main/java/main/oneDimensionArray/OneDimension.java
@@ -8,6 +8,22 @@ public class OneDimension {
     4.delete ->O(n)
     * */
     public static void main(String[] args) {
-
+        int[] array = new int[5];
+        array[0] = 20;
+        array[1] = 14;
+        array[2] = 43;
+        array[3] = 1;
+        array[4] = 90;
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        Student[] students = new Student[4];
+        students[0] = new Student("Alisa", 19);
+        students[1] = new Student("Narmina", 20);
+        students[2] = new Student("Kamil", 18);
+        students[3] = new Student("Narmin", 15);
+        for(int i=0;i<students.length;i++){
+            System.out.println(students[i].toString());
+        }
     }
 }
---
+++ arrays/src/main/java/main/oneDimensionArray/Student.java
@@ -0,0 +1,27 @@
+package main.oneDimensionArray;
+
+public class Student {
+    private final String name;
+    private final Integer age;
+
+    public Student(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
