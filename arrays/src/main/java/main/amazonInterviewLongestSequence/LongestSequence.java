16bf67f1e748fb9c14a1df17fdf07f0df113341d
talibnabi <talibnabiyev@gmail.com>
2022-09-11 08:44:12 -0700

    commit

--- arrays/src/main/java/main/amazonInterviewLongestSequence/LongestSequence.java
+++ arrays/src/main/java/main/amazonInterviewLongestSequence/LongestSequence.java
@@ -7,7 +7,7 @@ public class LongestSequence {
 
     public static void main(String[] args) {
 
-        int[] arr = {1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0};//input
+        int[] arr = {1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1};//input
         //first convert the problem
         List<Integer> list = new ArrayList<Integer>();
         int sequenceOnesTotal = 0;
@@ -26,5 +26,19 @@ public class LongestSequence {
             }
         }
         System.out.println(list);
+        int maxSequence = 0;
+        for (int i = 0; i < list.size(); i += 2) {
+            int len = list.get(i);
+            if ((i + 1) < list.size()) {
+                len += 1;
+            }
+            if ((i + 2) < list.size()) {
+                len += list.get(i + 2);
+            }
+            if (len > maxSequence) {
+                maxSequence = len;
+            }
+        }
+        System.out.println("maxSequence: " + maxSequence);
     }
 }
