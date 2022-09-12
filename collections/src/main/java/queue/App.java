c3e327ec424bb0bd6ed23883e830795ca07fa04b
talibnabi <talibnabiyev@gmail.com>
2022-09-12 05:44:01 -0700

    commit

--- collections/src/main/java/queue/App.java
+++ collections/src/main/java/queue/App.java
@@ -1,7 +1,35 @@
 package queue;
 
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class App {
     public static void main(String[] args) {
+        //First In First Out (FIFO)
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(342);
+        queue.add(23);
+        queue.add(23);
+        queue.add(234);
+        queue.add(234);
+        queue.forEach(System.out::println);
+        System.out.println();
 
+        System.out.println(queue.poll());
+        System.out.println();
+
+        PriorityQueue<Integer> queue1 = new PriorityQueue<>();
+        queue1.add(3);
+        queue1.add(3);
+        queue1.add(43);
+        queue1.forEach(System.out::println);
+        System.out.println();
+
+        PriorityQueue<Employee> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(new Employee("Alisa", "Aslan", 19));
+        priorityQueue.add(new Employee("Aslan", "Aslanov", 23));
+        System.out.println(priorityQueue.poll().getName());
+        priorityQueue.forEach(System.out::println);
     }
 }
---
+++ collections/src/main/java/sortArrayOfObjects/App.java
@@ -0,0 +1,7 @@
+package sortArrayOfObjects;
+
+public class App {
+    public static void main(String[] args) {
+        
+    }
+}
