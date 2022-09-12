4859c350af4ad50c1c55dd04ba4904a31c17fbdc
talibnabi <talibnabiyev@gmail.com>
2022-09-12 10:28:43 -0700

    commit

---
+++ recursion/src/recursion/App.java
@@ -0,0 +1,31 @@
+package recursion;
+
+public class App {
+
+    public static void main(String[] args) {
+        solution(4);
+    }
+
+    //Stack Call Back (1)
+    public static void solution(int N) {
+        if (N <= 0) {
+            return;
+        }
+        StringBuilder str = new StringBuilder();
+        if (N % 2 == 0) {
+            str.append(".Cordiality.");
+        }
+        if (N % 3 == 0) {
+            str.append(".Test.");
+        }
+        if (N % 5 == 0) {
+            str.append(".Coders.");
+        }
+        if (str.length() == 0) {
+            System.out.println(N);
+        } else {
+            System.out.println(str.toString());
+        }
+        solution(N - 1);
+    }
+}
