ec62407ae7694277c4cada296346b6a82e4c7b4f
talibnabi <talibnabiyev@gmail.com>
2022-09-12 01:43:02 -0700

    commit

--- arrays/src/main/java/main/main/BigTechInterQuestionSecond.java
+++ arrays/src/main/java/main/main/BigTechInterQuestionSecond.java
@@ -1,5 +1,14 @@
 package main.main;
 
 public class BigTechInterQuestionSecond {
-    
+    private static int[] array = new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7};
+
+    private static int largestContainer(int[] array) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int num = largestContainer(array);
+    }
 }
