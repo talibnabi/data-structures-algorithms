5e724f4f9b67254d1833b81513821b4c277c87e3
talibnabi <talibnabiyev@gmail.com>
2022-09-12 03:12:03 -0700

    commit

--- collections/src/main/java/treeSetAndHashSet/App.java
+++ collections/src/main/java/treeSetAndHashSet/App.java
@@ -1,15 +1,27 @@
 package treeSetAndHashSet;
 
 import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class App {
     public static void main(String[] args) {
-        HashSet<String> hashSet = new HashSet<>();
+        Set<String> hashSet = new HashSet<>();//HashSet is not sorted
         hashSet.add("Lucifer");
         hashSet.add("Lucifer");
         hashSet.add("Simon");
         hashSet.add("Lucy");
         hashSet.add("Lucifer");
         hashSet.forEach(System.out::println);
+
+        System.out.println();
+
+        Set<String> treeSet = new TreeSet<>();//TreeSet is sorted and fast
+        treeSet.add("Lucifer");
+        treeSet.add("Lucifer");
+        treeSet.add("Simon");
+        treeSet.add("Lucy");
+        treeSet.add("Lucifer");
+        treeSet.forEach(System.out::println);
     }
 }
