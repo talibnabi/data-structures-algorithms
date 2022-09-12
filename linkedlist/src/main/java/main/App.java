efcec2e74d81b72c98ad751220d10f79dd726fd9
talibnabi <talibnabiyev@gmail.com>
2022-09-12 06:13:35 -0700

    commit

---
+++ linkedlist/src/main/java/main/App.java
@@ -0,0 +1,16 @@
+package main;
+
+
+public class App {
+    // value/pointer
+
+    //Search --> O(n)
+    //Access -> O(n)
+    //Remove --> O(n)
+    //Insert --> O(n)
+
+    //Double LinkedList
+    public static void main(String[] args) {
+
+    }
+}
