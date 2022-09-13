eb83e59d5aaf9ea71240f956cf17aa6d349e2eb4
talibnabi <talibnabiyev@gmail.com>
2022-09-13 03:37:33 -0700

    commit

--- recursion/src/recursion/App2.java
+++ recursion/src/recursion/App2.java
@@ -1,13 +1,22 @@
 package recursion;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class App2 {
     public static void main(String[] args) {
-        int[] A = {2,-1, 3, -1, 5};
+        int[] A = {2, -1, 3, -1, 5};
         int result = solution(A);
         System.out.println(result);
     }
 
+    static Set<Integer> visited = new HashSet<>();
+
     private static int length(int[] A, int index) {
+        if (visited.contains(index)) {
+            return 1;
+        }
+        visited.add(index);
         if (A[index] == -1) {
             return 1;
         }
