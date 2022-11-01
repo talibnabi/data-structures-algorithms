b6e7e6795fd1840d5c074552e51f0342ae7c9562
talibnabi <talibnabiyev@gmail.com>
2022-11-01 02:13:10 -0700

    commit

--- abb-DS-algo/src/com/company/SortArray.java
+++ abb-DS-algo/src/com/company/SortArray.java
@@ -1,20 +1,16 @@
 package com.company;
 
 
+import java.util.Arrays;
+
 public class SortArray {
 
     public static void main(String[] args) {
         int[] arr = {34, 3, 23, 34, 5, 6, 3, 2, 1};
         Integer[] arr2 = {34, 3, 23, 34, 5, 6, 3, 2, 1};
-        int[] sortedArr = sort(arr);
-//        for (int i = 0; i < sortedArr.length; i++) {
-//            System.out.println(arr[i]);
-//        }
 
-        Integer[] sortedArr2 = sort2(arr2);
-        for (int i = 0; i < sortedArr2.length; i++) {
-            System.out.println(arr[i]);
-        }
+//        System.out.println(Arrays.toString(sort2(arr2)));
+        System.out.println(Arrays.toString(sort3(arr2)));
     }
 
     //bubble sort
@@ -39,11 +35,25 @@ public class SortArray {
     private static Integer[] sort2(Integer[] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] > arr[i + 1]) {
+                if (arr[i] > arr[j]) {
                     int temp = arr[i];
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = temp;
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
+            }
+        }
+        return arr;
+    }
+
+
+    //bubble sort
+    private static Integer[] sort3(Integer[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                i=-1;
             }
         }
         return arr;
